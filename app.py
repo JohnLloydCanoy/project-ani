1a5c49e6dcd30480c8a56844f73d95e1a34ffdf5
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-20 18:51:58 +0800

    Setting Up: ready to run

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Set the page configuration
+st.title("HI Welcome to Project Ani 👋")
+st.write("System is online and running.")
