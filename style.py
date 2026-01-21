24f5dc7a9537deba0bfa59a800b81fa5940fdb2c
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-21 20:38:11 +0800

    updating the style again

--- style.py
+++ style.py
@@ -5,4 +5,17 @@ def global_css():
         background-color: #f0f2f6;
     }
     </style>
+    """ 
+    
+def header_css():
+    return """
+    <style>
+    .header {
+        font-size: 36px;
+        font-weight: bold;
+        color: #4A90E2;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    </style>
     """
